04d05f79c0e83228d8dbaee1e3108b905bb9e344
Krisztian Balog <krisztian.balog@uis.no>
2021-06-14 10:33:04 +0200

    Add Ontology class

---
+++ dialoguekit/core/ontology.py
@@ -0,0 +1,23 @@
+"""Interface representing a domain ontology."""
+
+import yaml
+import os
+
+
+class Ontology:
+    """Represents a domain ontology."""
+
+    def __init__(self, config_file: str) -> None:
+        """Initializes the domain ontology.
+
+        Args:
+            config_file: Name of YAML config file.
+        """
+        if not os.path.isfile(config_file):
+            raise FileNotFoundError(f"Config file not found: {config_file}")
+        with open(config_file) as yaml_file:
+            self._config = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+    def get_slot_names(self):
+        """Returns the list of slot names."""
+        return list(self._config["slot_names"].keys())
---
+++ tests/core/test_ontology.py
@@ -0,0 +1,15 @@
+"""Tests for the DialogueHistory class."""
+
+import pytest
+
+from dialoguekit.core.ontology import Ontology
+
+
+# Sample ontology to be shared across multiple test cases.
+@pytest.fixture
+def sample_ontology():
+    return Ontology("tests/data/ontology.yaml")
+
+
+def test_slot_names(sample_ontology):
+    assert sample_ontology.get_slot_names() == ["TITLE", "GENRE", "ACTOR"]
