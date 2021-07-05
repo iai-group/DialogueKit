eb2d34787064b3fe2fdb64cf29501a36032fa69e
imsure318 <imsure318@163.com>
2021-07-05 12:30:17 +0100

    Cosine-similarity based intent classifier #22

--- dialoguekit/core/intent.py
+++ dialoguekit/core/intent.py
@@ -4,6 +4,14 @@
 class Intent:
     """Represents an intent."""
 
-    def __init__(self) -> None:
-        """Initializes the intent."""
-        pass
+    def __init__(self, label: str) -> None:
+        """Initializes the intent.
+
+        Args:
+            Label: intent label.
+        """
+        self.__label = label
+
+    @property
+    def label(self) -> str:
+        return self.__label
