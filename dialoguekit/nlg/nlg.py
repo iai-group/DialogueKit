1bb402659ec28b599c8947f84878e258b0bf1789
Shuo Zhang <imsure318@163.com>
2021-07-05 23:53:40 +0100

    first pass

--- dialoguekit/nlg/nlg.py
+++ dialoguekit/nlg/nlg.py
@@ -4,6 +4,7 @@ class NLG:
     def __init__(self) -> None:
         """Initializes the NLG component."""
         pass
+
     def generate_utterance_text(self, intent, slot_values) -> str:
         """Turns a structured utterance into a textual one."""
         pass
