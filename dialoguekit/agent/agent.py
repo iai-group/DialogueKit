a58f47cf6981b40700376c642b76757215546a3c
AleksanderD <Aleksander.m.drzewiecki@gmail.com>
2022-03-22 09:59:22 +0100

    Agent inherits from Participant

--- dialoguekit/agent/agent.py
+++ dialoguekit/agent/agent.py
@@ -6,16 +6,10 @@ agents.
 An agent instance needs to be connected with a DialogueManager by invoking
 `register_dialogue_manager()`.
 """
-
 from __future__ import annotations
-from abc import ABC, abstractmethod
+from dialoguekit.participant import Participant
+from abc import abstractmethod
 from enum import Enum
-from typing import TYPE_CHECKING
-
-from dialoguekit.core.utterance import Utterance
-
-if TYPE_CHECKING:
-    from dialoguekit.manager.dialogue_manager import DialogueManager
 
 
 class AgentType(Enum):
@@ -25,35 +19,17 @@ class AgentType(Enum):
     WOZ = 1
 
 
-class Agent(ABC):
+class Agent(Participant):
     """Represents an agent."""
 
-    def __init__(
-        self, agent_id: str, agent_type: AgentType = AgentType.BOT
-    ) -> None:
+    def __init__(self, id: str, type: AgentType = AgentType.BOT) -> None:
         """Initializes the agent.
 
         Args:
-            agent_id: Agent ID.
-            agent_type: Agent type (default: BOT).
+            id: Agent ID.
+            type: Agent type (default: BOT).
         """
-        self.__agent_id = agent_id
-        self._agent_type = agent_type
-        self._dialogue_manager = None
-
-    @property
-    def agent_id(self):
-        return self.__agent_id
-
-    def connect_dialogue_manager(
-        self, dialogue_manager: DialogueManager
-    ) -> None:
-        """Connects the Dialogue Manager instance for the agent.
-
-        Args:
-            dialogue_manager: A DialogueManager instance.
-        """
-        self._dialogue_manager = dialogue_manager
+        super().__init__(id=id, type=type)
 
     @abstractmethod
     def welcome(self) -> None:
@@ -63,13 +39,4 @@ class Agent(ABC):
     @abstractmethod
     def goodbye(self) -> None:
         """Sends the agent's goodbye message."""
-        pass
-
-    @abstractmethod
-    def receive_user_utterance(self, utterance: Utterance) -> None:
-        """This method is called each time there is a new user utterance.
-
-        Args:
-            utterance: User utterance.
-        """
         pass
