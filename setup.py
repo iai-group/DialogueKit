c211944ea8ebe3d15b0b6afdf980cdb4e53fc9c2
AleksanderD <Aleksander.m.drzewiecki@gmail.com>
2022-03-30 13:44:23 +0200

    Initial setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DialogueKit",
+    version="0.0.1",
+    author="Jafar Afzali, Krisztian Balog, Aleksander Drzewiecki \
+        and Shuo Zhang",
+    author_email="author@example.com",
+    description="This is a test of packaging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iai-group/dialoguekit",
+    project_urls={
+        "Bug Tracker": "https://github.com/iai-group/dialoguekit/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
