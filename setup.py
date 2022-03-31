897033d228b2690498bd0f73929d8b3e70768356
AleksanderD <Aleksander.m.drzewiecki@gmail.com>
2022-03-31 14:29:12 +0200

    change name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DialogueKit",
+    name="dialoguekit",
     version="0.0.1",
     author="Jafar Afzali, Krisztian Balog, Aleksander Drzewiecki \
         and Shuo Zhang",
