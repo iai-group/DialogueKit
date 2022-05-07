b4502f248863dd4588926f9e7013c8f5902b800e
AleksanderD <Aleksander.m.drzewiecki@gmail.com>
2022-05-07 15:41:54 +0200

    Add support for load and save

--- tests/nlu/test_intent_classifier_cosine.py
+++ tests/nlu/test_intent_classifier_cosine.py
@@ -75,3 +75,25 @@ def test_get_intent_similar_patterns(
         utterance = Utterance(utterance_text)
         predicted_intent = intent_classifier.get_intent(utterance)
         assert predicted_intent.label == intent.label
+
+
+def test_save_and_load_model(tmp_path, intents, utterances_1, labels_1):
+    save_to_dir = tmp_path
+    full_path = save_to_dir.absolute()
+    my_path = full_path.as_posix()
+
+    intent_classifier = IntentClassifierCosine(intents)
+    intent_classifier.train_model(utterances_1, labels_1)
+    intent_classifier.save_model(file_path=my_path + "/")
+
+    intent_classifier = None
+    intent_classifier = IntentClassifierCosine(intents)
+    intent_classifier.load_model(file_path=my_path + "/")
+
+    for utterance_template, intent in zip(utterances_1, labels_1):
+        utterance_text = utterance_template.text.replace(
+            PLACEHOLDER, "RANDOM_ITEM"
+        )
+        utterance = Utterance(utterance_text)
+        predicted_intent = intent_classifier.get_intent(utterance)
+        assert predicted_intent.label == intent.label
