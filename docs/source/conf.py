16bb939a16a4d95b6f06521e99341e66d4dc6f96
Nolwenn <28621493+NoB0@users.noreply.github.com>
2022-12-12 11:36:46 +0100

    Test documentation versioning. (#170)
    * Test documentation versioning.
    
    * black
    
    * Linting
    
    * black
    
    * Update branch whitelist.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,14 +41,20 @@ extensions = [
     "sphinx.ext.githubpages",
     "autoapi.extension",
     "myst_parser",
+    "sphinx_multiversion",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
+html_sidebars = {
+    "**": [
+        "versions.html",
+    ],
+}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
 source_suffix = [".rst", ".md"]
 
 # The encoding of source files.
@@ -88,3 +94,18 @@ autoapi_options = [
     "show-module-summary",
 ]
 autoapi_python_class_content = "init"
+
+# -- Options for versioning -------------------------------------------------
+# See documentation at:
+# https://holzhaus.github.io/sphinx-multiversion/master/configuration.html#
+smv_tag_whitelist = r"^.*$"  # Include all tags
+smv_branch_whitelist = r"^main$"  # Include only main branch
+smv_remote_whitelist = (
+    r"^(origin|upstream)$"  # Use branches from origin and upstream
+)
+smv_released_pattern = r"^tags/.*$"  # Tags only
+smv_outputdir_format = "{ref.name}"  # Use the branch/tag name
+
+# Determines whether remote or local git branches/tags are preferred if their
+# output dirs conflict
+smv_prefer_remote_refs = True
