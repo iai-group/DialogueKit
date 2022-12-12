0806017d584c0a5c6f5cc543629be689bdc72723
Nolwenn <28621493+NoB0@users.noreply.github.com>
2022-12-12 10:09:28 +0100

    black

--- dialoguekit/participant/__init__.py
+++ dialoguekit/participant/__init__.py
@@ -2,8 +2,4 @@
 from dialoguekit.participant.participant import DialogueParticipant, Participant
 from dialoguekit.participant.user_preferences import UserPreferences
 
-__all__ = [
-    "Participant",
-    "DialogueParticipant",
-    "UserPreferences"
-]
+__all__ = ["Participant", "DialogueParticipant", "UserPreferences"]
