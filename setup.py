8a3e981be01f438ba2d3f7cdfe4dd89e14fd882c
Nolwenn <28621493+NoB0@users.noreply.github.com>
2022-12-14 11:13:10 +0100

    add author

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ packages = setuptools.find_packages(where="dialoguekit")
 setuptools.setup(
     name="dialoguekit",
     version="0.0.7",
-    author="Jafar Afzali, Krisztian Balog, Aleksander Drzewiecki \
-        and Shuo Zhang",
+    author="Jafar Afzali, Krisztian Balog, Nolwenn Bernard, \
+        Aleksander Drzewiecki and Shuo Zhang",
     author_email="author@example.com",
     description=(
         "Toolkit for building conversational information access systems."
