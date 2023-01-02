9291817d575a6cd1c6e46842355e62bc23213e53
Nolwenn <28621493+NoB0@users.noreply.github.com>
2023-01-02 17:05:05 +0100

    Fix docstrings for NLU class Fixes #28

--- dialoguekit/nlu/slot_annotator_dict.py
+++ dialoguekit/nlu/slot_annotator_dict.py
@@ -2,11 +2,11 @@
 
 
 class SlotAnnotatorDict:
-    def __init__(self, slot_name, sf_dictionary) -> None:
+    def __init__(self, slot_name: str, sf_dictionary) -> None:
         """Initializes the class.
 
         Args:
-            slot_name: Name of the slot (Domain class).
+            slot_name: Name of the slot.
             sf_dictionary: Surface form dictionary.
         """
         pass
