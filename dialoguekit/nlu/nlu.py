c27b1a53948e1274325382f0c879e9385e13ed54
Nolwenn <28621493+NoB0@users.noreply.github.com>
2023-04-03 10:29:26 +0200

    Address review comment

--- dialoguekit/nlu/nlu.py
+++ dialoguekit/nlu/nlu.py
@@ -1,11 +1,12 @@
 """Natural language understanding."""
 
-from typing import Any, List
+from typing import List
 
+from dialoguekit.core.annotation import Annotation
 from dialoguekit.core.intent import Intent
-from dialoguekit.core.slot_value_annotation import SlotValueAnnotation
 from dialoguekit.core.utterance import Utterance
 from dialoguekit.nlu.intent_classifier import IntentClassifier
+from dialoguekit.nlu.slot_annotator import SlotAnnotator
 
 
 class NLU:
@@ -27,16 +28,14 @@ class NLU:
         """Classifies the intent of a given agent utterance."""
         return self._intent_classifier.classify_intent(utterance)
 
-    def annotate_slot_values(
-        self, utterance: Utterance
-    ) -> List[SlotValueAnnotation]:
+    def annotate_slot_values(self, utterance: Utterance) -> List[Annotation]:
         """Annotates a given utterance with slot annotators.
 
         Args:
             utterance: Utterance to annotate.
 
         Returns:
-            List of slot value annotations.
+            List of annotations.
         """
         annotation_list = []
         for slot_annotator in self._slot_annotators:
