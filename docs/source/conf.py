c43fadcd52262cb5fcd4ead2dbc1266880b26330
Nolwenn <28621493+NoB0@users.noreply.github.com>
2026-06-02 16:38:53 +0200

    Fixes #276 and #273

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ author = "Jafar Afzali, Krisztian Balog, Aleksander Drzewiecki \
         and Shuo Zhang"
 
 # The short X.Y version.
-version = "0.1.0"
+version = "0.1.2"
 # The full version, including alpha/beta/rc tags.
-release = "0.1.0"
+release = "0.1.2"
 
 # -- General configuration ---------------------------------------------------
 
